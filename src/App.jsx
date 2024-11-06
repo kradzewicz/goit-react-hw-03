2100fcbc61632da5e55df7a435f303999ad0410b
Kacper Radzewicz <kacper.radzewicz.26@gmail.com>
2024-11-06 22:06:21 +0100

    first commit
    final project before revision

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,92 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+/** @format */
+
+import { useState, useEffect } from "react";
+import "./App.css";
+import * as Yup from "yup";
+import { nanoid } from "nanoid";
+import { ContactFrom } from "./components/ContactForm";
+import { SearchBox } from "./components/SearchBox";
+import { ContactList } from "./components/ContactList";
+import { isObject } from "formik";
+
+const initialContactList = [
+  { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
+  { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
+  { id: "id-3", name: "Eden Clements", number: "645-17-79" },
+  { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
+];
+
+const initialValues = {
+  name: "",
+  number: "",
+};
+
+const validationSchema = Yup.object().shape({
+  name: Yup.string()
+    .min(2, "Name is too short!")
+    .max(50, "Name is too long!")
+    .required("This field is required!"),
+  number: Yup.string()
+    .min(2, "number is too short!")
+    .max(50, "number is too long!")
+    .required("This field is required!"),
+});
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [contactList, setContactList] = useState(() => {
+    const startContactList = JSON.parse(localStorage.getItem("ContactList"));
+    return startContactList === null
+      ? initialContactList
+      : typeof startContactList === "object"
+      ? startContactList
+      : initialContactList;
+  });
+  const [filter, setFilter] = useState("");
+
+  useEffect(() => {
+    localStorage.setItem("ContactList", JSON.stringify(contactList));
+  }, [contactList]);
+
+  const nameFieldId = nanoid();
+  const numberFieldId = nanoid();
+
+  const handleDelete = (id) => {
+    setContactList((p) => p.filter((contact) => contact.id !== id));
+  };
+
+  const handleAdd = (values, actions) => {
+    const addContact = (newContact) => {
+      setContactList((prev) => [...prev, newContact]);
+    };
+    addContact({ id: nanoid(), name: values.name, number: values.number });
+    actions.resetForm();
+  };
+
+  const handleFilterChange = (e) => {
+    setFilter(e.target.value);
+  };
+
+  const visibleContacts = contactList.filter((contact) =>
+    contact.name.toLowerCase().includes(filter.toLowerCase())
+  );
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Phone Book</h1>
+      <ContactFrom
+        initialValues={initialValues}
+        handleAdd={handleAdd}
+        nameId={nameFieldId}
+        numberId={numberFieldId}
+        validationSchema={validationSchema}
+      />
+      <SearchBox handleFilterChange={handleFilterChange} value={filter} />
+      <ContactList
+        initialContacts={visibleContacts}
+        handleDelete={handleDelete}
+      />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
